857280c9f064199075b82ccdc0cd17593a6918f7
ErnestArth <ernest5arthur@gmail.com>
2025-08-07 14:33:07 +0000

    refactored get all team members

--- src/main/java/com/leadstracker/leadstracker/repositories/TeamTargetRepository.java
+++ src/main/java/com/leadstracker/leadstracker/repositories/TeamTargetRepository.java
@@ -2,11 +2,13 @@ package com.leadstracker.leadstracker.repositories;
 
 import com.leadstracker.leadstracker.entities.TeamTargetEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TeamTargetRepository extends JpaRepository<TeamTargetEntity, Long> {
 
     // Get all targets for a team where due date is in the future or today
